03f29977241ff4721cca59b9a2b30df030f00780
Mohamed said <kankgood@gmail.com>
2024-04-12 18:17:01 +0200

    Localization for Categories and Brand

--- src/app/components/shared/brands/brands.component.ts
+++ src/app/components/shared/brands/brands.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { IBrand } from '../../../Models/ibrand';
 import { RouterModule } from '@angular/router';
 import { CarouselModule } from 'primeng/carousel';
+import { LocalizationService } from '../../../Services/localiztionService/localization.service';
 
 @Component({
   selector: 'app-brands',
@@ -16,6 +17,12 @@ export class BrandsComponent {
   @Input() brands: IBrand[]=[] ;
   @Input() isSmall=false;
   numOfBrands!: number;
+
+  isArabic!: boolean;
+
+  constructor(private localizationService: LocalizationService) {
+    this.localizationService.IsArabic.subscribe((ar) => (this.isArabic = ar));
+  }
   ngOnInit(): void {
     if (this.isSmall)
       this.numOfBrands=8;
