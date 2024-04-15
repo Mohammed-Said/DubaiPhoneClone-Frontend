f0ef277da8a0e6e866d7768dbfa5a6637c10112d
Mohamed said <kankgood@gmail.com>
2024-04-15 04:20:24 +0200

    Order and Payment Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,13 +10,18 @@ import { LoginComponent } from './components/user/login/login.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ProductDetailsComponent } from './components/products/product-details/product-details.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ShippingComponent } from './components/checkout/shipping/shipping.component';
+import { PaymentComponent } from './components/checkout/payment/payment.component';
 
 export const routes: Routes = [
 
     {path:"",component:HomeComponent},
     {path:"sign up",component:SignupComponent,title:"sign up"},
     {path:"login",component:LoginComponent,title:"login"},
-    {path:"checkout",component:CheckoutComponent},
+    {path:"checkout",component:CheckoutComponent,children:[
+      {path:"",component:ShippingComponent},
+      {path:"payment",component:PaymentComponent},
+    ]},
     {path:"cart",component:CartComponent},
     {path:"category" ,component:ProductsPageComponent,children:[
       {path:":category",component:ProductsPageComponent},
